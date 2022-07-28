0ff54a55b04470104fcd8c0cd90b71b9ec795bbd
Daniel <109466382+lsxol@users.noreply.github.com>
2022-07-28 17:30:02 +0200

    Add files via upload

---
+++ part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class IntegerInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        String num = scanner.nextLine();
+        int intNum = Integer.valueOf(num);
+        System.out.println("You gave the number " + intNum);
+
+    }
+}
